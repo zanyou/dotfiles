7ceb0670d1d04cc095d45a8b822e976ff3355ae9
Tomohito Ozaki <ozaki@yuroyoro.com>
2013-11-07 18:13:25 +0900

    Add initialization of vim with NeoBundleInstall ( ꒪⌓꒪)

--- setup.sh
+++ setup.sh
@@ -10,3 +10,5 @@ done
 # install oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
+
