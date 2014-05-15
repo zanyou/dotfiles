c0e3fa5dc62ab2a0fff233d8c8ecc8a1b0fbdcb1
Tomohito Ozaki <ozaki@yuroyoro.com>
2014-05-15 17:43:14 +0900

    ʕ　 ﾟ皿ﾟ ʔ do not run NeoBundleInstall on setup.sh

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ done
 # install oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && vim -c ':NeoBundleInstall'
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
 
