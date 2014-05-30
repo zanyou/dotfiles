92495cfb5604c6027d013f8dd5485e6459e84cb8
Tomohito Ozaki <ozaki@yuroyoro.com>
2014-05-30 14:25:43 +0900

    ʕ　 ﾟ皿ﾟ ʔ Add percol settings
    [mooz/percol](https://github.com/mooz/percol)
    
    [anything 的な絞りこみコマンド percol の紹介 - 備忘録](http://d.hatena.ne.jp/kbkbkbkb1/20120429/1335835500)
    [ターミナル版anything的なpercolをzawの代わりに試してみた - $shibayu36->blog;](http://shibayu36.hatenablog.com/entry/2013/10/06/184146)
    [percol 入れて zsh と組み合わせたら超便利](https://gist.github.com/mitukiii/4234173)
    [一人で使えるGithub Issue](http://azu.github.io/slide/udonjs/github-issue.html)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc)
+DOT_FILES=( .zsh .zshrc .zshrc.custom .zshrc.alias .zshrc.linux .zshrc.osx .ctags .emacs.el .gdbinit .gemrc .gitconfig .gitignore .inputrc .irbrc .sbtconfig .screenrc .vimrc .vim .vrapperrc import.scala .tmux.conf .dir_colors .rdebugrc .pryrc .percol.d)
 
 for file in ${DOT_FILES[@]}
 do
