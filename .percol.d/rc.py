789ce2e2d3358a62cda7494e36b53bd9ac9926a5
Tomohito Ozaki <ozaki@yuroyoro.com>
2014-07-16 14:23:48 +0900

    Remove rsense

--- .percol.d/rc.py
+++ .percol.d/rc.py
@@ -14,7 +14,7 @@ percol.import_keymap({
     "C-y" : lambda percol: percol.command.yank(),
     "C-n" : lambda percol: percol.command.select_next(),
     "C-p" : lambda percol: percol.command.select_previous(),
-    "C-j" : lambda percol: percol.command.select_next(),
+    "C-j" : lambda percol: percol.finish(),
     "C-k" : lambda percol: percol.command.select_previous(),
     "C-v" : lambda percol: percol.command.select_next_page(),
     "M-v" : lambda percol: percol.command.select_previous_page(),
@@ -22,6 +22,8 @@ percol.import_keymap({
     "M->" : lambda percol: percol.command.select_bottom(),
     "C-m" : lambda percol: percol.finish(),
     "C-g" : lambda percol: percol.cancel(),
+    "C-s" : lambda percol: percol.command.toggle_mark_and_next(),
+    "C-u" : lambda percol: percol.command.unmark_all(),
     "M-c" : lambda percol: percol.command.toggle_case_sensitive(),
     "M-m" : lambda percol: percol.command.toggle_finder(FinderMultiQueryMigemo),
     "M-r" : lambda percol: percol.command.toggle_finder(FinderMultiQueryRegex)
